d49ab74b407646fcc2caab81f8d9cfc2430096b3
tom <tom-fletcher@outlook.com>
2020-10-22 15:42:52 +0100

    code to provision 1 control server and 3 web servers in AWS.

---
+++ ec2-build.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.70"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = var.region
+}
+
+resource "aws_instance" "ansible-control-server" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  tags = {
+    Name = "ansible-control"
+  }
+}
+
+resource "aws_instance" "ansible-web-servers" {
+  count         = length(var.web_servers)
+  ami           = var.ami
+  instance_type = var.instance_type
+  tags = {
+    Name = element(var.web_servers, count.index)
+  }
+}
---
+++ vars.tf
@@ -0,0 +1,20 @@
+variable "region" {
+  default = "eu-west-2"
+}
+
+variable "ami" {
+  default = "ami-05c424d59413a2876"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "web_servers" {
+  type = list(string)
+  default = [
+    "ansible-web-server-1",
+    "ansible-web-server-2",
+    "ansible-web-server-3"
+  ]
+}
